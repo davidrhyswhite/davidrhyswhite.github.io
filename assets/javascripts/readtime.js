16f93e6d3e2feda898356b3669c757f9c7bda919
David White <david.white@spry-soft.com>
2013-11-19 22:29:34 +0000

    jQueryified read time JavaScript to work across many articles.

--- assets/javascripts/readtime.js
+++ assets/javascripts/readtime.js
@@ -1,19 +1,27 @@
+$.fn.addReadTime = function () {
+  return this.each(function () {
+    var wordCount = parseInt($(this).attr('content')),
+        seconds = (wordCount / 5);
+
+    var readTime = function () {
+      if (seconds < 44) {
+        return "less than a minute";
+      } else if (seconds < 59) {
+        return "about 1 minute";
+      } else {
+        var number_of_minutes = Math.round(seconds / 60);
+        var plural = (number_of_minutes < 2) ? "" : "s";
+        return "about " + number_of_minutes + " minute" + plural;
+      }
+    };
+
+    $(this).after(readTime());
+  });
+}
+
+
 $(document).ready(function () {
-  var prop = $('meta[itemprop="wordCount"]'),
-      wordCount = parseInt(prop.attr('content')),
-      seconds = (wordCount / 5);
 
-  var readTime = function () {
-    if (seconds < 44) {
-      return "less than a minute";
-    } else if (seconds < 59) {
-      return "about 1 minute"
-    } else {
-      var number_of_minutes = Math.round(seconds / 60);
-      var plural = (number_of_minutes < 2) ? "" : "s";
-      return "about " + number_of_minutes + " minute" + plural;
-    }
-  }
+  $('meta[itemprop="wordCount"]').addReadTime();
 
-  prop.after(readTime());
 });
